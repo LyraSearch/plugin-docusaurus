d95862a9c38a593aa36013ad4a2111580dbf23c0
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-10-14 10:02:14 +0200

    feat(client): base search bar

--- src/client/theme/SearchBar/SearchBar.jsx
+++ src/client/theme/SearchBar/SearchBar.jsx
@@ -1,3 +1,12 @@
-const SearchBar = () => {}
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import useFetchIndex from './hooks/useFetchIndex'
+
+const SearchBar = () => {
+  const { siteConfig } = useDocusaurusContext()
+
+  const { index, isLoading } = useFetchIndex(siteConfig)
+
+  console.log(index, isLoading)
+}
 
 export default SearchBar
