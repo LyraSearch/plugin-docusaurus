ee9af21a80d314ffafef80c6fcee2160a98c91bb
Carlos Espa <carlos.espa@nearform.com>
2022-12-23 14:36:44 +0100

    chore: website migrated to typescript

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import clsx from 'clsx'
-import styles from './styles.module.css'
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
 
-const FeatureList = [
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
   {
     title: 'Easy to Use',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
@@ -11,7 +17,7 @@ const FeatureList = [
         Docusaurus was designed from the ground up to be easily installed and
         used to get your website up and running quickly.
       </>
-    )
+    ),
   },
   {
     title: 'Focus on What Matters',
@@ -21,7 +27,7 @@ const FeatureList = [
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
         ahead and move your docs into the <code>docs</code> directory.
       </>
-    )
+    ),
   },
   {
     title: 'Powered by React',
@@ -31,11 +37,11 @@ const FeatureList = [
         Extend or customize your website layout by reusing React. Docusaurus can
         be extended while reusing the same header and footer.
       </>
-    )
-  }
-]
+    ),
+  },
+];
 
-function Feature({ Svg, title, description }) {
+function Feature({title, Svg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
@@ -46,10 +52,10 @@ function Feature({ Svg, title, description }) {
         <p>{description}</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default function HomepageFeatures() {
+export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
@@ -60,5 +66,5 @@ export default function HomepageFeatures() {
         </div>
       </div>
     </section>
-  )
+  );
 }
